49300311287e74bf5ec3526ca8382ce36f3923e7
yusys-cloud <yangzq80@gmail.com>
2023-06-18 22:33:23 +0800

    feat(proxy): init commit

--- llms/chatglm.py
+++ llms/chatglm.py
@@ -9,11 +9,13 @@
 # print(response)
 
 from transformers import AutoTokenizer, AutoModel
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-tokenizer.save_pretrained("/home/ubuntu/yzq/models/chatglm-6b/tokenizer")
+# tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("/home/ubuntu/yzq/models/chatglm-6b/tokenizer", trust_remote_code=True)
+# tokenizer.save_pretrained("/home/ubuntu/yzq/models/chatglm-6b/tokenizer")
 
-model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
-model.save_pretrained("/home/ubuntu/yzq/models/chatglm-6b/models")
+# model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+model = AutoModel.from_pretrained("/home/ubuntu/yzq/models/chatglm-6b/models",trust_remote_code=True).half().cuda()
+# model.save_pretrained("/home/ubuntu/yzq/models/chatglm-6b/models")
 
 response, history = model.chat(tokenizer, "你好，介绍下go功能特点", history=[])
 print(response)
